906530782c11df40d01e945d5e1d3ee1a28a02b1
giselleLago <gisellelago25994@gmail.com>
2020-08-10 16:03:36 +0200

    Add project files.

---
+++ CW_AreTheSame/AreTheSame.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CW_AreTheSame
+{
+    public class AreTheSame
+    {
+        public bool comp(int[] a, int[] b)
+        {
+            if (a == null || b == null)
+            {
+                return false;
+            }
+            var copy = a.Select(x => x * x).ToArray();
+            Array.Sort(copy);
+            Array.Sort(b);
+
+            return copy.SequenceEqual(b);
+
+
+            /*if (a == null || b == null)
+            {
+                return false;
+            }
+            var newList = new List<int>();
+            foreach (var x in a)
+            {
+                foreach (var y in b)
+                {
+                    if (x * x == y)
+                    {
+                        newList.Add(y);
+                        break;
+                    }
+                }
+            }
+            var newB = b.ToList();
+            newB.Sort();
+            newList.Sort();
+            if (newList.SequenceEqual(newB) && newList.Count == a.Count() && a != null && b != null)
+            {
+                return true;
+            }
+            return false;*/
+        }
+    }
+}
---
+++ CW_AreTheSame/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CW_AreTheSame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var a = new AreTheSame();
+            Console.WriteLine(a.comp(new int[] { 121, 144, 19, 161, 19, 144, 19, 11 }, new int[] { 121, 14641, 20736, 361, 25921, 361, 20736, 361 }));
+            Console.ReadKey();
+        }
+    }
+}
